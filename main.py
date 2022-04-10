7a777fd7831846297890b2290658823de35a53d8
Sebastian Waisbrot <seppo0010@gmail.com>
2022-04-10 10:34:33 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import json
+
+import requests
+from feedgen.feed import FeedGenerator
+
+fg = FeedGenerator()
+fg.title('SAIJ: Jurisprudencia CSJN')
+fg.link(href='http://www.saij.gob.ar/')
+fg.description('Tribunal: CORTE SUPREMA DE JUSTICIA DE LA NACION; Tipo de Documento: Jurisprudencia')
+
+URL = 'http://www.saij.gob.ar/busqueda?o=0&p=25&f=Total%7CFecha%7CEstado+de+Vigencia%5B5%2C1%5D%7CTema%5B5%2C1%5D%7COrganismo%5B5%2C1%5D%7CAutor%5B5%2C1%5D%7CJurisdicci%C3%B3n%5B5%2C1%5D%7CTribunal%2FCORTE+SUPREMA+DE+JUSTICIA+DE+LA+NACION%7CPublicaci%C3%B3n%5B5%2C1%5D%7CColecci%C3%B3n+tem%C3%A1tica%5B5%2C1%5D%7CTipo+de+Documento%2FJurisprudencia&s=fecha-rango%7CDESC&v=colapsada'
+
+r = requests.get(URL)
+data = r.json()
+for doc in data['searchResults']['documentResultList']:
+    abstract = json.loads(doc['documentAbstract'])
+    url = 'http://www.saij.gob.ar/{}/{}'.format(
+        abstract['document']['metadata']['friendly-url']['description'],
+        abstract['document']['metadata']['uuid'],
+    )
+
+    url = 'http://www.saij.gob.ar/view-document?guid={}'.format(
+        abstract['document']['metadata']['uuid'],
+    )
+    r = requests.get(url)
+    data = json.loads(r.json()['data'])
+
+    url = 'http://www.saij.gob.ar/busqueda?r=id-infojus%3ASU{}&o=0&p=500&f=Total'.format(
+        data['document']['content']['sumarios-relacionados']['sumario-relacionado'][0],
+    )
+    r = requests.get(url)
+    data = r.json()
+
+    res = data['searchResults']['documentResultList'] 
+    if len(res) == 0:
+        continue
+    url = 'http://www.saij.gob.ar/view-document?guid={}'.format(
+        res[0]['uuid'],
+    )
+    r = requests.get(url)
+    data = json.loads(r.json()['data'])
+    fe = fg.add_entry()
+    fe.id(url)
+    fe.link(href=url)
+    fe.title(data['document']['content']['sumario'])
+    fe.description(data['document']['content']['texto'])
+
+print(fg.rss_str(pretty=True))
