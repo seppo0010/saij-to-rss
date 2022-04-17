12d60153baea0cb4e38cd923590deb33e541d19a
Sebastian Waisbrot <seppo0010@gmail.com>
2022-04-17 16:41:24 -0300

    Send messages to telegram

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from github import Github, GithubIntegration
 import requests
 from feedgen.feed import FeedGenerator
+import telegram
 
 orgname, reponame = os.environ['PUBLISH_GITHUB_REPOSITORY'].split('/')
 
@@ -17,12 +18,19 @@ g = Github(ghi.get_access_token(
 ).token)
 repo = g.get_repo(os.environ['PUBLISH_GITHUB_REPOSITORY'])
 
+bot = telegram.Bot(token=os.environ['PUBLISH_TELEGRAM_TOKEN'])
+def send_tg(url, summary, text):
+    bot.send_message(chat_id=os.environ['PUBLISH_TELEGRAM_CHAT_ID'], text=f'{url}\n\n{summary}\n\n{text}')
+
 fg = FeedGenerator()
 fg.title('SAIJ: Jurisprudencia CSJN')
 fg.link(href='http://www.saij.gob.ar/')
 fg.description('Tribunal: CORTE SUPREMA DE JUSTICIA DE LA NACION; Tipo de Documento: Jurisprudencia')
 
 URL = 'http://www.saij.gob.ar/busqueda?o=0&p=25&f=Total%7CFecha%7CEstado+de+Vigencia%5B5%2C1%5D%7CTema%5B5%2C1%5D%7COrganismo%5B5%2C1%5D%7CAutor%5B5%2C1%5D%7CJurisdicci%C3%B3n%5B5%2C1%5D%7CTribunal%2FCORTE+SUPREMA+DE+JUSTICIA+DE+LA+NACION%7CPublicaci%C3%B3n%5B5%2C1%5D%7CColecci%C3%B3n+tem%C3%A1tica%5B5%2C1%5D%7CTipo+de+Documento%2FJurisprudencia&s=fecha-rango%7CDESC&v=colapsada'
+
+sent_file = repo.get_contents("sent.json", ref="database")
+sent = json.loads(sent_file.decoded_content)
 
 r = requests.get(URL)
 data = r.json()
@@ -45,7 +53,7 @@ for doc in data['searchResults']['documentResultList']:
     r = requests.get(url)
     data = r.json()
 
-    res = data['searchResults']['documentResultList'] 
+    res = data['searchResults']['documentResultList']
     if len(res) == 0:
         continue
     url = 'http://www.saij.gob.ar/view-document?guid={}'.format(
@@ -54,10 +62,16 @@ for doc in data['searchResults']['documentResultList']:
     r = requests.get(url)
     data = json.loads(r.json()['data'])
     fe = fg.add_entry()
-    fe.id(url)
+    fe.id(permaurl)
     fe.link(href=permaurl)
     fe.title(data['document']['content']['sumario'])
     fe.description(data['document']['content']['texto'])
+
+    if url not in sent:
+        sent.append(url)
+        send_tg(permaurl, data['document']['content']['sumario'], data['document']['content']['texto'])
+
+repo.update_file(sent_file.path, "Update sent", json.dumps(sent), sent_file.sha, branch="database")
 
 rss = fg.rss_str(pretty=True)
 contents = repo.get_contents("rss.xml", ref="gh-pages")
